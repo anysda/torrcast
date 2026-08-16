bf8c96529bbb039a2d79a8d3ae480fa823bb0315
anysda <anysdaw@gmail.com>
2026-08-16 22:00:21 +0300

    Clear traces of orphaned modules from the root package

--- torrcast/__init__.py
+++ torrcast/__init__.py
@@ -1,17 +1,11 @@
 """Публичный API torrcast: версия и реэкспорт ошибок."""
 
-from typing import Any
-
 from torrcast.domain.infra_error import InfraError
 from torrcast.domain.not_found_error import NotFoundError
 from torrcast.domain.swarm_error import SwarmError
 from torrcast.domain.torrcast_error import TorrcastError
 from torrcast.domain.version import __version__
 from torrcast.domain.why import why
-
-# Аннотации сохраняют прежний типовой API без файлов-обёрток.
-commands_legacy: Any
-doctor_checks: Any
 
 __all__ = [
     "InfraError",
