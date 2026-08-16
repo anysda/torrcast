b073538d314dcd1de5131abb949eefdda255ee98
anysda <anysdaw@gmail.com>
2026-08-16 22:00:48 +0300

    Drop the test for a module that no longer exists

--- tests/test_doctor_checks.py
+++
@@ -1,7 +0,0 @@
-"""Совместимый набор системных проб сохраняет прежний вход."""
-
-from torrcast import doctor_checks
-
-
-def test_doctor_checks_keeps_checkup() -> None:
-    assert callable(doctor_checks.checkup)
