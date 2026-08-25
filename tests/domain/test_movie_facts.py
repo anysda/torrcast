f3cea90728affa772c01d29babb9c5b6d056713e
anysda <anysdaw@gmail.com>
2026-08-25 11:43:52 +0300

    Remove picture facts from the old help-text scaffold

--- tests/domain/test_movie_facts.py
+++
@@ -1,7 +0,0 @@
-"""Tests for the immutable movie-facts value."""
-
-from torrcast.domain.movie_facts import MovieFacts
-
-
-def test_keeps_optional_movie_metadata() -> None:
-    assert MovieFacts("Тачки", "Cars", 2006).original_title == "Cars"
