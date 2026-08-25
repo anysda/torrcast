91502a6b59fca6351293a0e6da549cc86aba9c0e
anysda <anysdaw@gmail.com>
2026-08-25 11:40:44 +0300

    Drop the range-read port re-export for the domain contract

--- tests/ports/test_range_reader.py
+++
@@ -1,8 +0,0 @@
-"""Проверяет экспорт порта диапазонного чтения."""
-
-from torrcast.ports.range_reader import RangeReader
-
-
-def test_port_is_available() -> None:
-    """Порт ссылается на транспортно-независимый контракт."""
-    assert RangeReader.__name__ == "RangeReader"
--- torrcast/ports/range_reader.py
+++
@@ -1,5 +0,0 @@
-"""Открывает сценариям диапазонное чтение внешнего ресурса."""
-
-from torrcast.domain.frames.range_reader import RangeReader
-
-__all__ = ["RangeReader"]
