dfb7eaa74767613469c79d93f19f732488da85cf
anysda <anysdaw@gmail.com>
2026-08-25 11:40:29 +0300

    Remove a dead-code cluster spanning feed use case to server address

--- tests/domain/test_server_address.py
+++
@@ -1,7 +0,0 @@
-"""Tests for the started-server address value."""
-
-from torrcast.domain.server_address import ServerAddress
-
-
-def test_keeps_public_base_url() -> None:
-    assert ServerAddress("http://host:8080").base_url == "http://host:8080"
