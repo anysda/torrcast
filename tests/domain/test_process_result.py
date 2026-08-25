b97246580f434c08e203cae9626df1b9594f4b62
anysda <anysdaw@gmail.com>
2026-08-25 11:43:52 +0300

    Remove external-program launch left without callers

--- tests/domain/test_process_result.py
+++
@@ -1,7 +0,0 @@
-"""Tests for the completed-process value."""
-
-from torrcast.domain.process_result import ProcessResult
-
-
-def test_defaults_captured_streams_to_empty() -> None:
-    assert ProcessResult(0).stdout == ""
